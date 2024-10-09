3fd6bfd57b0a4b688dde1ce35cc1df9dc41dab72
Legit-Sam <webdeveloper2167@gmail.com>
2024-10-09 20:45:26 +0100

    Final

---
+++ drizzle.config.js
@@ -0,0 +1,12 @@
+export default {
+  dialect: "postgresql",
+  schema: "./utils/db/schema.ts",
+  out: "./drizzle",
+  dbCredentials: {
+      url: "postgresql://contentdb_owner:hDw3yeGkQ6BT@ep-winter-night-a5oo775i.us-east-2.aws.neon.tech/threaddb?sslmode=require",
+      connectionString:
+        "postgresql://contentdb_owner:hDw3yeGkQ6BT@ep-winter-night-a5oo775i.us-east-2.aws.neon.tech/threaddb?sslmode=require",
+    },
+  };
+  
+
